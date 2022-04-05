7512baecadda14c9e5da165bb7c65908e310ff2d
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2022-04-05 13:21:53 +0100

    Mobile Search

--- src/features/layout/MobileSearch/Search.js
+++ src/features/layout/MobileSearch/Search.js
@@ -5,10 +5,15 @@ import { closeSearchModal } from './SearchModalSlice';
 import styles from './styles.module.css';
 import ArrowBackIcon from '../../../assets/icons/arrow-back.svg';
 import SearchIcon from '../../../assets/icons/search.svg';
+import { useNavigate } from 'react-router-dom';
+import { changeSearchVariable } from '../../pages/Search/SearchVariableSlice';
 
 function Search({ onClose = () => null, ...props }) {
 	const dispatch = useDispatch();
 	const searchModalState = useSelector((state) => state.searchModal.open);
+
+	let navigate = useNavigate();
+	const searchVariable = useSelector((state) => state.searchVariable.value);
 	return (
 		<Modal open={searchModalState} onClose={() => dispatch(closeSearchModal())}>
 			<div className={styles.navbar} onClick={(e) => e.stopPropagation()}>
@@ -25,9 +30,11 @@ function Search({ onClose = () => null, ...props }) {
 						type='text'
 						className={styles.searchInput}
 						placeholder='Search books, genres, authors, etc.'
+						value={searchVariable}
+						onChange={(e) => dispatch(changeSearchVariable(e.target.value))}
 					/>
 					<div className={styles.searchIconContainer}>
-						<img src={SearchIcon} alt='' />
+						<img src={SearchIcon} alt='' onClick={() => navigate('/search')} />
 					</div>
 				</div>
 			</div>
